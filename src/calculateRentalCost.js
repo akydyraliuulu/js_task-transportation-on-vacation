8ab4d71147191bbdebd245ea472feff1a3e91dee
Appaz Kydyraliuulu <akydyraliuulu@gmail.com>
2025-03-18 06:21:44 +0900

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,19 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  let totalCost = 0;
+
+  for (let i = 1; i <= days; i++) {
+    totalCost += 40;
+  }
+
+  if (days >= 7) {
+    totalCost -= 50;
+  } else if (days >= 3) {
+    totalCost -= 20;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
